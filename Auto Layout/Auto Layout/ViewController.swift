a22af484175a2d108fe374a658e5c772b3ac27d5
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-10-31 16:44:12 -0400

    Lesson 1 complete

--- Auto Layout/Auto Layout/ViewController.swift
+++ Auto Layout/Auto Layout/ViewController.swift
@@ -10,29 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Adding a closure for the bear image view
+    let bearImageView: UIImageView = {
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "bear_first"))
+        
+        // enables AutoLayout
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let textDescriptor: UITextView = {
+       let textView = UITextView()
+        textView.text = "Join us today in our Fun & Games!"
+        textView.font = UIFont.boldSystemFont(ofSize: 18) // bold
+        textView.textAlignment = .center // centered
+        textView.isEditable = false
+        textView.isScrollEnabled = false
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // set the background color for the view
-//        view.backgroundColor = .yellow
+        // view.backgroundColor = .yellow
         
-        // Set an image to stay inside of a UIImageView
-        let imageView = UIImageView(image: #imageLiteral(resourceName: "bear_first"))
-        view.addSubview(imageView) //add image as a subview
-        //imageView.frame = CGRect(x: 0, y: 0, width: 100, height: 100) // set position + size
+        view.addSubview(bearImageView) //add image as a subview
+        view.addSubview(textDescriptor) // add first line of text
         
-        // enables the following constraints
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        
-        // set constraints for X + Y
-        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        // imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        // add image to be 100px from the top instead
-        imageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
-        
-        // add height / width constraints
-        imageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
-        imageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        setupLayout()
     }
 
     override func didReceiveMemoryWarning() {
@@ -40,6 +46,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // Making things more clean & OO
+    func setupLayout() {
+        // set constraints for X + Y
+        bearImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+   
+        // add image to be 100px from the top instead
+        bearImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+    
+        // add height / width constraints
+        bearImageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        bearImageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        
+        // Add text constrints
+        textDescriptor.topAnchor.constraint(equalTo: bearImageView.bottomAnchor, constant: 120).isActive = true
+        textDescriptor.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        textDescriptor.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        textDescriptor.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+        
+    }
 
 }
-
