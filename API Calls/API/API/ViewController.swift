2952706087890997c5094981c3d892f412cf1ec1
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-10-31 23:34:10 -0400

    Array of items working now

--- API Calls/API/API/ViewController.swift
+++ API Calls/API/API/ViewController.swift
@@ -19,16 +19,40 @@ struct Data: Decodable {
     var body: String
 }
 
+struct Course: Decodable {
+    var id: Int
+    var name: String
+    var link: String
+    var imageUrl: String
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        Create some fake data to get the idea
+        // Create some fake data to get the idea - Understanding the Object types for JSON
         let myData = Data(userId: 1, id: 1,
                           title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
                           body: "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto")
-//        print(myData)
+        
+        singleJSONItem()
+        
+        
+        // Array of JSON Items Found at https://api.letsbuildthatapp.com/jsondecodable/courses
+        arrayOfJSON()
+        
+        //perform something a little more in depth
+        complexJSON()
+ 
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func singleJSONItem() {
         
         // STEP 2 - Fetch JSON from endpoint
         let jsonURLString = "https://jsonplaceholder.typicode.com/posts/1"
@@ -37,11 +61,11 @@ class ViewController: UIViewController {
         // Create a Session to visit the address specified above
         URLSession.shared.dataTask(with: url) { (data, response, err) in
             // check the err or that the response == 200
-            
+        
             guard let data = data else { return }
             let dataAsString = String(data: data, encoding: .utf8)
             //print(dataAsString)
-            
+        
             // STEP 3 - Serialize the data
             do {
                 // decode the JOSN and serialize -- Thanks Swit 4 ;)
@@ -51,19 +75,36 @@ class ViewController: UIViewController {
                 print("Error Serializing: ", jsonErr)
             }
         }.resume()
- 
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     
+    func arrayOfJSON() {
+        
+        // STEP 2 - Fetch JSON from endpoint
+        let jsonURLString = "https://api.letsbuildthatapp.com/jsondecodable/courses"
+        guard let url = URL(string: jsonURLString) else { return }
+        
+        // Create a Session to visit the address specified above
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+            // check the err or that the response == 200
+            
+            guard let data = data else { return }
+        
+            // STEP 3 - Serialize the data
+            do {
+                let courses = try JSONDecoder().decode([Course].self, from: data) // the bracket is important
+                print(courses) // Prints an array of type JSON for Course items
+            } catch let jsonErr {
+                print("Error Serializing: ", jsonErr)
+            }
+        }.resume()
+    }
     
-    
-    
-    
-
+    // 2 items outside an array of items
+    func complexJSON() {
+        
+        let jsonURLString = "https://api.letsbuildthatapp.com/jsondecodable/website_description"
+        
+    }
     
 }
