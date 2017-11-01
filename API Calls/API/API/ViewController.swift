f8eb61d77d7c57999bd3ded76efcf1644a0c117c
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-10-31 23:17:19 -0400

    Addressing endpoint configured - JSON parsed & printed

--- API Calls/API/API/ViewController.swift
+++ API Calls/API/API/ViewController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-// STEP 1
 // using fake API endpoint found at https://jsonplaceholder.typicode.com/
+
+// STEP 1
 // Create a STRUCT to help decode the data as it's recieved
-struct Data: Codable {
+struct Data: Decodable {
     var userId: Int
     var id: Int?
     var title: String
@@ -25,10 +26,32 @@ class ViewController: UIViewController {
         
 //        Create some fake data to get the idea
         let myData = Data(userId: 1, id: 1,
-      title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-      body: "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto")
+                          title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+                          body: "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto")
+//        print(myData)
         
-        print(myData)
+        // STEP 2 - Fetch JSON from endpoint
+        let jsonURLString = "https://jsonplaceholder.typicode.com/posts/1"
+        guard let url = URL(string: jsonURLString) else { return }
+        
+        // Create a Session to visit the address specified above
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+            // check the err or that the response == 200
+            
+            guard let data = data else { return }
+            let dataAsString = String(data: data, encoding: .utf8)
+            //print(dataAsString)
+            
+            // STEP 3 - Serialize the data
+            do {
+                // decode the JOSN and serialize -- Thanks Swit 4 ;)
+                let item = try JSONDecoder().decode(Data.self, from: data)
+                print(item.title)
+            } catch let jsonErr {
+                print("Error Serializing: ", jsonErr)
+            }
+        }.resume()
+ 
     }
 
     override func didReceiveMemoryWarning() {
@@ -40,7 +63,7 @@ class ViewController: UIViewController {
     
     
     
-    // STEP 2 - Fetch JSON
+    
 
     
 }
