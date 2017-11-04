10c79b92bbb301e91f3559e83dae0cac4a7effbc
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-11-04 14:09:41 -0400

    Adding buttons down below - Lesson 3

--- Auto Layout/Auto Layout/ViewController.swift
+++ Auto Layout/Auto Layout/ViewController.swift
@@ -32,8 +32,8 @@ class ViewController: UIViewController {
         
         // Add more text to the end of the above text
         attributedText.append(NSAttributedString(string: "\n\n\nAre you ready for loads and loads of fun? Don't wait any longer! We hope to see you in our stores soon.",
-                                                 attributes: [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 12),
-                                                              NSAttributedStringKey.foregroundColor: UIColor.gray]))
+                            attributes: [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 12),
+                                         NSAttributedStringKey.foregroundColor: UIColor.gray]))
         
         
         textView.attributedText = attributedText
@@ -45,12 +45,30 @@ class ViewController: UIViewController {
         return textView
     }()
     
+    // Add previous button - private so that no other .swift classes can access this
+    private let previousButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("PREV", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.gray, for: .normal)
+        return button
+    }()
+    
+    // Add previous button - private so that no other .swift classes can access this
+    private let nextButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("NEXT", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.gray, for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.addSubview(textDescriptor) // add first line of text
-        
         setupLayout()
+        setupBottomControls()
     }
 
     override func didReceiveMemoryWarning() {
@@ -88,5 +106,32 @@ class ViewController: UIViewController {
         // constant on right / opposite must be negative
         textDescriptor.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
         textDescriptor.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+    
+    //using FilePrivate because the init of the button is private - this preserves the privacy
+    fileprivate func setupBottomControls() {
+//        view.addSubview(previousButton)
+        
+        let greenView = UIView()
+        greenView.backgroundColor = .green
+        
+        // Using UI Stack view for adding buttons to the bottom - much more effiencent & easy
+        let bottomControlsStackView = UIStackView(arrangedSubviews:
+            [previousButton, greenView, nextButton])
+        
+        bottomControlsStackView.distribution = .fillEqually // Tells thestack view to split
+        view.addSubview(bottomControlsStackView)
+        
+        // enable autoLayout
+        bottomControlsStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Easier way of activating constraints - I like this more
+        NSLayoutConstraint.activate([
+            bottomControlsStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            bottomControlsStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor), // safe for landscape
+            bottomControlsStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            bottomControlsStackView.heightAnchor.constraint(equalToConstant: 50),
+            
+        ])
     }
 }
