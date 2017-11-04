ff7c3e188e25eb894a8492bc5001d4c253eda9f3
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-11-04 14:59:23 -0400

    Starting Lesson 4

---
+++ Auto Layout/Auto Layout/SwipingController.swift
@@ -0,0 +1,18 @@
+//
+//  swipingController.swift
+//  Auto Layout
+//
+//  Created by Wyatt Baggett on 11/4/17.
+//  Copyright © 2017 Thrive Community Church. All rights reserved.
+//
+
+import UIKit
+
+class SwipingController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView?.backgroundColor = .green
+    }
+}
