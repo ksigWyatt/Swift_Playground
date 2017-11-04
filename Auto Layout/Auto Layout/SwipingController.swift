7357771e3934647c234543539435fd626e5e64a0
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-11-04 15:11:04 -0400

    It's looking messy but It'll get there

--- Auto Layout/Auto Layout/SwipingController.swift
+++ Auto Layout/Auto Layout/SwipingController.swift
@@ -14,5 +14,23 @@ class SwipingController: UICollectionViewController {
         super.viewDidLoad()
         
         collectionView?.backgroundColor = .green
+        // add this line to prevent NSInternalInconsistencyException & register cells
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellId")
+        
+    }
+    
+    // section - An index number identifying a section in collectionView. 
+    override func collectionView(_ collectionView: UICollectionView,
+                                 numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    // add cells for the sections above
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt
+                                 indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId",
+                                                         for: indexPath)
+        cell.backgroundColor = .red
+        return cell
     }
 }
