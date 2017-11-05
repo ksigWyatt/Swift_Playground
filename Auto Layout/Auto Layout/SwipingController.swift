1ef6e4b2e40c9d3250e379a7a863b864bf1a4728
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-11-04 20:39:11 -0500

    Protocol Oriented Programming

--- Auto Layout/Auto Layout/SwipingController.swift
+++ Auto Layout/Auto Layout/SwipingController.swift
@@ -11,29 +11,6 @@ import UIKit
 // CONTROLLER
 // controls the each cell and how they are controlled
 class SwipingController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
-
-    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
-        
-        // using coordinator to preserve the index of the item for the page that we are on
-        // while transitioning this fires
-        coordinator.animate(alongsideTransition: { (_) in
-            self.collectionViewLayout.invalidateLayout()
-            
-            // Check for issues with the pages & correct them - namely the first page
-            if self.pageControl.currentPage == 0 {
-                self.collectionView?.contentOffset = .zero
-            }
-            else {
-                // scroll to correct index while transitioning
-                let indexPath = IndexPath(item: self.pageControl.currentPage, section: 0)
-                self.collectionView?.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
-            }
-            
-            
-        }) { (_) in
-            
-        }
-    }
     
     // don't just use Arrays - they crash easily if there are too few or many cells vs count    
     let pages = [
@@ -59,7 +36,7 @@ class SwipingController: UICollectionViewController, UICollectionViewDelegateFlo
     }()
     
     // Dots at the bottom for viewing the page we are on & what not
-    private lazy var pageControl: UIPageControl = {
+    lazy var pageControl: UIPageControl = {
         let pc = UIPageControl()
         pc.currentPage = 0
         pc.numberOfPages = pages.count // access member of your class thanks to lazy var
@@ -108,7 +85,6 @@ class SwipingController: UICollectionViewController, UICollectionViewDelegateFlo
         pageControl.currentPage = Int(pageID) // set the dot to be the current page
     }
     
-    
     // MARK: Start
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -119,48 +95,6 @@ class SwipingController: UICollectionViewController, UICollectionViewDelegateFlo
         // add this line to prevent NSInternalInconsistencyException & register cells
         collectionView?.register(PageCell.self, forCellWithReuseIdentifier: "cellId") // adding custom View Cell - this is important too
         collectionView?.isPagingEnabled = true // allows for snaps between the cells
-    }
-    
-    // add spacing function for the cells to display properly w/out the while line
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout:
-        UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 0
-    }
-    
-    // section - An index number identifying a section in collectionView. 
-    override func collectionView(_ collectionView: UICollectionView,
-                                 numberOfItemsInSection section: Int) -> Int {
-        // OO # of pages
-        return pages.count
-    }
-    
-    // add cells for the sections above
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt
-                                 indexPath: IndexPath) -> UICollectionViewCell {
-        
-        // access PageCell items - CASTING is important
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId",
-                                                         for: indexPath) as! PageCell
-        /*
-           # of the row in the collection View
-           following the modulo if the index path is divisibly by 2 then set the color to red else green
-           nice little inline coonditionals using an optional *!!THIS IS IMPORTANT!!*
-        
-            cell.backgroundColor = indexPath.item % 2 == 0 ? .red : .green
-        */
-        
-        // changes the image of the image view declared in bearImageView to change it based
-        // on the image found at the name in the global struct of Pages
-        let page = pages[indexPath.item]
-        cell.page = page // Swiping sets this page property
-        
-        return cell
-    }
-    
-    // set the collection view to take the entire size of the screen
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout:
-            UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: view.frame.height)
     }
     
     //using FilePrivate because the init of the button is private - this preserves the privacy
