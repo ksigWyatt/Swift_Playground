6b75cdd4da65e44deef66bebe1ad5779a43128b2
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-11-06 10:34:10 -0500

    Well  lets get this party started

--- iManage Inventory/iManage Inventory/InventoyTableView.swift
+++ iManage Inventory/iManage Inventory/InventoyTableView.swift
@@ -8,7 +8,43 @@
 
 import UIKit
 
-class InventoryTableView: UITableView {
+class InventoryTableView: UITableViewController {
+    
+    var items: [Int] = [Int]()
+    var currentIndex: Int = 0
+    let newItem = "New  Item"
+    let itemTitle: String = "item"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+        loadDummyData()
+        
+    }
+    
+//    @objc func addNewItem(_ sender: AnyObject) {
+//
+//        if objects.count == 0 || objects[0] != newItem {
+//
+//            objects.insert(newItem, at: 0)
+//            let indexPath = IndexPath(row: 0, section: 0)
+//            self.tableView.insertRows(at: [indexPath], with: .automatic)
+//
+//        }
+//        saveViewState()
+//
+//        currentIndex = 0
+//        self.performSegue(withIdentifier: "updateItems", sender: self)
+//    }
+//
+//    func saveViewState() {
+//        UserDefaults.standard.set(objects, forKey: itemTitle)
+//        UserDefaults.standard.synchronize()
+//    }
+//
 
-
+    func loadDummyData() {
+        
+    }
+    
 }
