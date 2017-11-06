bfd43d3bb9201c32b0cbcfce257d26f3abe5e49d
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-11-06 12:22:28 -0500

    Pre- lunch commit

--- iManage Inventory/iManage Inventory/InventoyTableView.swift
+++ iManage Inventory/iManage Inventory/InventoyTableView.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class InventoryTableView: UITableViewController {
     
-    var items: [Int] = [Int]()
+    var items: [String] = [String]()
     var currentIndex: Int = 0
     let newItem = "New  Item"
     let itemTitle: String = "item"
@@ -20,29 +20,36 @@ class InventoryTableView: UITableViewController {
      
         loadDummyData()
         
+        self.navigationItem.leftBarButtonItem = self.editButtonItem
+        
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add,
+                                        target: self,
+                                        action: #selector(addNewItem(_:)))
+        self.navigationItem.rightBarButtonItem = addButton
+        
     }
     
-//    @objc func addNewItem(_ sender: AnyObject) {
-//
-//        if objects.count == 0 || objects[0] != newItem {
-//
-//            objects.insert(newItem, at: 0)
-//            let indexPath = IndexPath(row: 0, section: 0)
-//            self.tableView.insertRows(at: [indexPath], with: .automatic)
-//
-//        }
-//        saveViewState()
-//
-//        currentIndex = 0
-//        self.performSegue(withIdentifier: "updateItems", sender: self)
-//    }
-//
-//    func saveViewState() {
-//        UserDefaults.standard.set(objects, forKey: itemTitle)
-//        UserDefaults.standard.synchronize()
-//    }
-//
+    @objc func addNewItem(_ sender: AnyObject) {
 
+        // If there's no items OR there is not an item called "New Item"
+        if items.count == 0 || items[0] != newItem {
+
+            items.insert(newItem, at: 0)
+            let indexPath = IndexPath(row: 0, section: 0)
+            self.tableView.insertRows(at: [indexPath], with: .automatic)
+            saveViewState()
+        }
+        currentIndex = 0
+        self.performSegue(withIdentifier: "updateItems", sender: self)
+    }
+    
+    //
+    func saveViewState() {
+        UserDefaults.standard.set(items, forKey: itemTitle)
+        UserDefaults.standard.synchronize()
+    }
+    
+    //
     func loadDummyData() {
         
     }
--- iManage Inventory/iManage Inventory/UpdateItemViewController.swift
+++ iManage Inventory/iManage Inventory/UpdateItemViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class UpdateItemViewController: UIViewController {
-
+    
+    @IBOutlet weak var currentStock: UILabel!
+    var quantity: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,26 @@ class UpdateItemViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func recieved(_ sender: Any) {
+        quantity += 100
+        currentStock.text = "\(quantity)"
+    }
+    
+    @IBAction func consumed(_ sender: Any) {
+        quantity -= 100
+        
+        if (quantity < 0) {
+            
+            let alert = UIAlertController(title: "Alert!",
+                                          message: "There can't be negative stock - Unless you sold it too well ;)",
+                                          preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Okay",
+                                          style: UIAlertActionStyle.default,
+                                          handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            quantity = 0
+        }
+        currentStock.text = "\(quantity)"
+    }
 }
 
