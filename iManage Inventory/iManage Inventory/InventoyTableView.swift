6f00b917aaa309688937e6d6ae64623b1bdf9b76
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-11-06 10:16:59 -0500

    Project init

---
+++ iManage Inventory/iManage Inventory/InventoyTableView.swift
@@ -0,0 +1,14 @@
+//
+//  FirstViewController.swift
+//  iManage Inventory
+//
+//  Created by Wyatt Baggett on 11/6/17.
+//  Copyright © 2017 Wyatt Baggett. All rights reserved.
+//
+
+import UIKit
+
+class InventoryTableView: UITableView {
+
+
+}
